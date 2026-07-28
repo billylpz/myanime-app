0235a12faf8b5bd428e7e07d62304eb6b44f52ae
billylpz <billyj.96@hotmail.com>
2026-07-28 15:44:39 -0500

    refactor(pokemon): move pipes to folders

--- src/app/pokemons/pipes/pokemon-name/pokemon-name.pipe.ts
+++ src/app/pokemons/pipes/pokemon-name/pokemon-name.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Pokemon } from '../interfaces/pokemon.interface';
+import { Pokemon } from '../../interfaces/pokemon.interface';
 
 @Pipe({
   name: 'pokemonName'
